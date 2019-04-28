6fda7eb2e3d5b9851af6498d427aea3f2743b7b0
Ruchir.Agrawal <>
2019-04-28 16:06:40 +0530

    Course Code Challenges 2 & 3 Solution

---
+++ UdemyCC2.py
@@ -0,0 +1,17 @@
+# Udemy's Python Course Coding Challenge 2
+# To create a List containing 5 elements
+
+foodItems = ['Ice Cream', 'Sandwich', 'Thaali', 'Pao Bhaji', 'Momos']
+
+# To print the third element of the list
+
+print(foodItems[2])
+
+# To add an element to the list and display the list
+
+foodItems.append('Kulfi')
+print(foodItems)
+
+# To insert element named Tacos at third position and print the list
+foodItems.insert(2, 'Tacos')
+print(foodItems)
---
+++ UdemyCC3.py
@@ -0,0 +1,19 @@
+# Udemy's Python Course Coding Challenge 3
+
+# To use for loop and range function in Python
+
+for i in range(0, 5):
+    print('I am a programmer')
+
+
+# To create a function that which displays the square of values from 1 to 9
+
+def def_square():
+    for j in range(1, 10):
+        print(j*j)
+
+
+def_square()
+
+
+
